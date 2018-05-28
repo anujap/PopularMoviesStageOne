1a9361df596cc4363d2b361a6e6c66e74320f99d
anujap <anujapatki87@gmail.com>
2018-05-27 19:41:37 -0500

    f_commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+To change the API Key,
+
+open the MovieUtils.java file. This file is in the (com.example.anuja.popularmoviesstageone.webservice) package.
+
+Replace your key for the constant "API_KEY"
--- app/src/main/java/com/example/anuja/popularmoviesstageone/webservice/MovieUtils.java
+++ app/src/main/java/com/example/anuja/popularmoviesstageone/webservice/MovieUtils.java
@@ -11,7 +11,7 @@ public class MovieUtils {
     public static final String MOVIE_PATH = "3/movie/";
     public static final String API_KEY_PARAM = "api_key";
     public static final String ENDPOINT_PARAM = "endpoint";
-    public static final String API_KEY = "eea971c8e6615a1794951f8ff0ae6f2b";
+    public static final String API_KEY = "";
     public static final String ENDPOINT_POPULARITY = "popular";
     public static final String ENDPOINT_TOP_RATED = "top_rated";
 
